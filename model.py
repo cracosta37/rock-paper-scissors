2924799b0994526d64d1e8353b732c89d7ee117e
CarlosRDeveloper <carlosroberto.acosta@gmail.com>
2024-08-05 10:44:02 -0500

    Included the models in model.py

--- model.py
+++ model.py
@@ -3,3 +3,18 @@ def score_model(prev_play, opponent_history=[]):
     bad_response = {'S': 'P', 'P': 'R', 'R': 'S'}
     
     return
+
+def model0(prev_play, opponent_history=[]):
+    return
+
+def model1(prev_play, opponent_history=[]):
+    return
+
+def model2(prev_play, opponent_history=[]):
+    return
+
+def model3(prev_play, opponent_history=[]):
+    return
+
+def model4(prev_play, opponent_history=[]):
+    return
