70d4b38049d2bb1b360451de02ad18cc0fb0752f
CarlosRDeveloper <carlosroberto.acosta@gmail.com>
2024-08-05 10:42:16 -0500

    Corrected a mistake

--- model.py
+++ model.py
@@ -1,6 +1,5 @@
-def score_model(prev_play, opponent_history=[])
-    
+def score_model(prev_play, opponent_history=[]):
     ideal_response = {'P': 'S', 'R': 'P', 'S': 'R'}
     bad_response = {'S': 'P', 'P': 'R', 'R': 'S'}
-
+    
     return
