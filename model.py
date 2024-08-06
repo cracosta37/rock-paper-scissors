849c3825e5864712b5e559857e7f2af3071cedb1
CarlosRDeveloper <carlosroberto.acosta@gmail.com>
2024-08-06 10:00:35 -0500

    Added an if statement and started working on the for loop

--- model.py
+++ model.py
@@ -1,6 +1,14 @@
-def score_model(prev_play, opponent_history=[]):
+def score_model(prev_play, opponent_history=[]): 
+    if len(opponent_history) == 0:
+        return float(-1.0)
+    
     ideal_response = {'P': 'S', 'R': 'P', 'S': 'R'}
-    bad_response = {'S': 'P', 'P': 'R', 'R': 'S'}
+    worse_response = {'S': 'P', 'P': 'R', 'R': 'S'}
+
+    model_record = []
+    w_max = 0
+    for i in range(len(opponent_history)):
+        return
     
     return
 
