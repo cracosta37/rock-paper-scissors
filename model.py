1307856257a93d356589fd468163371509d83749
CarlosRDeveloper <carlosroberto.acosta@gmail.com>
2024-08-08 10:51:13 -0500

    Finished score_model function

--- model.py
+++ model.py
@@ -1,5 +1,8 @@
-def score_model(prev_play, opponent_history=[]): 
-    if len(opponent_history) == 0:
+import numpy as np
+from random import randint
+
+def score_model(model, opponent_history=[]): 
+    if len(opponent_history) < 2:
         return float(-1.0)
     
     ideal_response = {'P': 'S', 'R': 'P', 'S': 'R'}
@@ -7,22 +10,29 @@ def score_model(prev_play, opponent_history=[]):
 
     model_record = []
     w_max = 0
-    for i in range(len(opponent_history)):
-        return
-    
+    for i in range(1, len(opponent_history)):
+        if model(opponent_history[:i]) == ideal_response[opponent_history[i]]:
+            model_record.append(i**2)
+        elif model(opponent_history[:i]) == worse_response[opponent_history[i]]:
+            model_record.append(-i**2)
+        w_max += i**2
+
+        if w_max == 0:
+            return float(0.0)
+   
+    return np.sum(model_record) / w_max
+
+def model0(opponent_history=[]):
     return
 
-def model0(prev_play, opponent_history=[]):
+def model1(opponent_history=[]):
     return
 
-def model1(prev_play, opponent_history=[]):
+def model2(opponent_history=[]):
     return
 
-def model2(prev_play, opponent_history=[]):
+def model3(opponent_history=[]):
     return
 
-def model3(prev_play, opponent_history=[]):
-    return
-
-def model4(prev_play, opponent_history=[]):
+def model4(opponent_history=[]):
     return
