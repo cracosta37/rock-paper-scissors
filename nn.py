55683fd06322003bfea393289a9f81c33674329b
CarlosRDeveloper <carlosroberto.acosta@gmail.com>
2024-08-20 14:13:26 -0500

    Extracted the data to a pd data frame

--- nn.py
+++ nn.py
@@ -1,3 +1,6 @@
 import numpy as np
+import pandas as pd
 import tensorflow as tf
 
+data_from_record122 = pd.read_csv('Data/rps-record122.csv')
+data_from_record_dtclf = pd.read_csv('Data/rps-record_dtclf')
