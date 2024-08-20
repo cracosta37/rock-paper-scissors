2152b94ff9757f9bf86edb5e5e34438358d0ca44
CarlosRDeveloper <carlosroberto.acosta@gmail.com>
2024-08-20 14:30:55 -0500

    Created constants.py and initialized labels array

---
+++ constants.py
@@ -0,0 +1,1 @@
+NUMBER_OF_IMPUTS = 7
--- nn.py
+++ nn.py
@@ -2,5 +2,13 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 
+import constants as c
+
 data_from_record122 = pd.read_csv('Data/rps-record122.csv')
 data_from_record_dtclf = pd.read_csv('Data/rps-record_dtclf')
+
+num_rounds = 0
+
+lables = np.empty((num_rounds, c.NUMBER_OF_IMPUTS ))
+
+
