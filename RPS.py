2e0cc83bf202337d1a0f24691362b75af456d902
CarlosRDeveloper <carlosroberto.acosta@gmail.com>
2024-12-04 16:54:14 -0500

    Changed model for models

--- RPS.py
+++ RPS.py
@@ -1,5 +1,5 @@
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
-from model import score_model, model0, model1, model2, model3, model4
+from models import score_model, model0, model1, model2, model3, model4
 
 def player(prev_play, opponent_history=[]):
     opponent_history.append(prev_play)
