7e78b34b4d497f30c7a00cba4e20b2e6443f1caa
cracosta37 <carlosroberto.acosta@gmail.com>
2025-02-26 11:36:37 -0500

    Modifyed data_generator.py to include player_history

--- data_generator.py
+++ data_generator.py
@@ -6,7 +6,7 @@ from RPS import player
 import constants as c
 
 players = [mrugesh, abbey, quincy, kris]
-record = pd.DataFrame(columns=['game_id', 'round', 'p1'])
+record = pd.DataFrame(columns=['game_id', 'round', 'p1', 'p2'])
 
 
 for i in range(4):
@@ -14,10 +14,12 @@ for i in range(4):
     temp_df = pd.DataFrame({
         'game_id': i,                  
         'round': range(c.NUMBER_OF_GAMES),          
-        'p1': player.opponent_history          
+        'p1': player.opponent_history,
+        'p2': player.player_history         
     })
     record = pd.concat([record, temp_df], ignore_index=True)
 
 record['p1'] = record['p1'].map({'R': 0, 'P': 1, 'S': 2})
+record['p2'] = record['p2'].map({'R': 0, 'P': 1, 'S': 2})
 
 record.to_csv('record.csv')
